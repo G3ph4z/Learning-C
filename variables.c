7dfa6eab9e4d62c0fe8720ca95c5f972b240c3f3
G3ph4z <info@denes.cloud>
2018-06-17 17:43:51 +0200

    Variables

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int count, number, star;	/* three integer vartiables */
+	float percent, total;		/* two float variables */
+
+	count = 1;
+
+	printf("Count: %i \n", count);
+}
