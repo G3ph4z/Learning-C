05f4f6e1c1e15fffc4a7601222199197fa9c3f75
G3ph4z <info@denes.cloud>
2018-06-17 17:51:49 +0200

    Added explanation to variables

--- variables.c
+++ variables.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
+/*
+ * Full names of data types
+ * ---------------------------------------------
+ * Full name		commonly used keyword
+ * ---------------------------------------------
+ *  char		signed char
+ *  short		signed short int
+ *  int			signed int
+ *  long		signed long int
+ *  long long		signed long long int
+ *  unsigned char	unsigned char
+ *  unsigned short	unsigned short int
+ *  unsigned int	unsigned int
+ *  unsigned long	unsigned long int
+ *  unsigned long long	unsigned long long int
+ *
+ *
+ *  Variable Declarations
+ * --------------------------------------------
+ *  typename varname;
+ */
+
+
 int main()
 {
 	int count, number, star;	/* three integer vartiables */
